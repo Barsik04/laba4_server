85b8d9ad193e8651c7693d157844aed3146f397c
prest <prestigio924@gmail.com>
2022-11-07 12:20:33 +0300

    firstCommit

---
+++ src/main/java/com/example/server/DataObject.java
@@ -0,0 +1,24 @@
+
+package com.example.server;
+
+public class DataObject
+{
+    private long id;
+    private String name;
+    public DataObject(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
